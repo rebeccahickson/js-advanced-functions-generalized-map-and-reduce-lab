d76eef624b9247daeeab2b9ed9e8eab71114636c
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-04 18:21:45 -0500

    commit tree cb82fa2ef7a80a4ff5ad6636c0d90ff09c1ca4d6

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(arr, func) {
+  //   return arr.map((item) => {
+  //     return func(item);
+  //   });
+  let newArr = [];
+  arr.forEach((element) => newArr.push(func(element)));
+  return newArr;
+}
+
+function reduce(arr, func, start) {
+  let x;
+  let i;
+  if (start) {
+    x = start;
+    i = 0;
+  } else {
+    x = arr[0];
+    i = 1;
+  }
+  for (; i < arr.length; i++) {
+    x = func(arr[i], x);
+  }
+  return x;
+}
